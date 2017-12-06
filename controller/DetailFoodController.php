4af3674ea1bd0f42a646357c8178455715bca642
Huong <Huong@Huong.local>
2017-12-06 20:38:53 +0700

    relatedFoods

--- controller/DetailFoodController.php
+++ controller/DetailFoodController.php
@@ -6,18 +6,20 @@ class DetailFoodController extends Controller{
     public function getDetailFood(){
         $alias = $_GET['url'];
         $model = new DetailFoodsModel;
-
         $food = $model->getDetailFood($alias);
-
         if(!$food){
             header("Location:404.php");
         }
-        // echo "<pre>";
-		// print_r($food);
-		// echo "</pre>";
-        // die;
+        
+        //$id_type = $food->id_type;
+        $relatedFood = $model->getRelatedFood($food->id_type,$food->id);
+        echo "<pre>";
+		print_r($relatedFood);
+		echo "</pre>";
+        die;
         $data = array(
-            'food'=>$food
+            'food'=>$food,
+            'relatedFood'=>$relatedFood
         );
         return $this->loadView("chi-tiet",$data);
     }
--- model/DetailFoodModel.php
+++ model/DetailFoodModel.php
@@ -11,4 +11,13 @@ class DetailFoodsModel extends DBConnect{
                 
         return $this->loadOneRow($sql);
     }
+
+    function getRelatedFood($id_type,$id_food){
+        $sql = "SELECT f.* FROM foods f 
+                INNER JOIN page_url p 
+                    ON f.id_url = p.id
+                WHERE f.id_type=$id_type
+                    AND f.id <> $id_food";
+        return $this->loadMoreRows($sql);
+    }
 }
